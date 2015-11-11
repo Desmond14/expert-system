9247f38316c97b3a11fdc9acf57fff4e4ca4c7ec
Desmond14 <s.lakomy@gmail.com>
2015-11-11 19:04:17 +0100

    Program now remembers negative answers

--- miasto_wies.pl
+++ miasto_wies.pl
@@ -1,6 +1,5 @@
 :- dynamic
-    xpozytywne/2,
-    xnegatywne/2.
+    zapamietane/3.
 
 powinien_mieszkac_w(ustrzyki_gorne) :- woli_mieszkac(na_wsi),
                         wskazana_bliskosc(gory).
@@ -21,50 +20,43 @@ woli_mieszkac(na_wsi) :- moze_mieszkac(na_wsi), spelnia_warunki(na_wsi).
 
 woli_mieszkac(w_miescie) :- spelnia_warunki(w_miescie).
 
-moze_mieszkac(na_wsi) :- pozytywne(czy, masz_prawo_jazdy).
-moze_mieszkac(na_wsi) :- negatywne(czy, praca_zwiazana_z_miastem).
+moze_mieszkac(na_wsi) :- pytaj(czy, masz_prawo_jazdy).
+moze_mieszkac(na_wsi) :- \+pytaj(czy, praca_zwiazana_z_miastem).
 
-spelnia_warunki(na_wsi) :- negatywne(czy, wolisz_bliskosc_kina_niz_lasu),
-                           pozytywne(czy, nie_przeszkadza_duza_odleglosc_do_sklepu).
+spelnia_warunki(na_wsi) :- \+pytaj(czy, wolisz_bliskosc_kina_niz_lasu),
+                           pytaj(czy, nie_przeszkadza_duza_odleglosc_do_sklepu).
 
-spelnia_warunki(w_miescie) :- pozytywne(czy, wolisz_bliskosc_kina_niz_lasu),
-                              pozytywne(czy, nie_przeszkadza_duzy_ruch).
+spelnia_warunki(w_miescie) :- pytaj(czy, wolisz_bliskosc_kina_niz_lasu),
+                              pytaj(czy, nie_przeszkadza_duzy_ruch).
 
-wskazana_bliskosc(gory) :- pozytywne(czy, lubisz_chodzic_po_gorach).
-wskazana_bliskosc(gory) :- pozytywne(czy, uprawiasz_sporty_zimowe).
+wskazana_bliskosc(gory) :- pytaj(czy, lubisz_chodzic_po_gorach).
+wskazana_bliskosc(gory) :- pytaj(czy, uprawiasz_sporty_zimowe).
 
-wskazana_bliskosc(woda) :- pozytywne(czy, lubisz_wypoczywac_na_plazy).
-wskazana_bliskosc(woda) :- pozytywne(czy, zeglujesz).
+wskazana_bliskosc(woda) :- pytaj(czy, lubisz_wypoczywac_na_plazy).
+wskazana_bliskosc(woda) :- pytaj(czy, zeglujesz).
 
-preferuje_dojazd(komunikacja_miejska) :- negatywne(czy, lubisz_jezdzic_autem).
-preferuje_dojazd(komunikacja_miejska) :- pozytywne(czy, nie_przeszkadzaja_tlumy),
-									     pozytywne(czy, lubisz_czytac_ksiazki).
-						
-pozytywne(X,Y) :- xpozytywne(X,Y), !.
+preferuje_dojazd(komunikacja_miejska) :- pytaj(czy, lubisz_jezdzic_autem).
+preferuje_dojazd(komunikacja_miejska) :- pytaj(czy, nie_przeszkadzaja_tlumy),
+									     pytaj(czy, lubisz_czytac_ksiazki).
 
-pozytywne(X,Y) :- \+xnegatywne(X,Y), pytaj(X,Y,tak).
-
-negatywne(X,Y) :- xnegatywne(X,Y), !.
-
-negatywne(X,Y) :- \+xpozytywne(X,Y), pytaj(X,Y,nie).
-
-pytaj(X,Y,tak) :- !, format('~w ~w ? (t/n)~n',[X,Y]),
-                    read(Reply),
-                    (Reply = 't'),
-                    pamietaj(X,Y,tak).
+pytaj(X,Y) :- zapamietane(X, Y, tak),
+              !.
+			  
+pytaj(X,Y) :- zapamietane(X, Y, _),
+              !,
+			  fail.
                     
-pytaj(X,Y,nie) :- !, format('~w ~w ? (t/n)~n',[X,Y]),
+pytaj(X,Y) :- format('~w ~w ? (t/n)~n',[X,Y]),
                     read(Reply),
-                    (Reply = 'n'),
-                    pamietaj(X,Y,nie).
-                    
-pamietaj(X,Y,tak) :- assertz(xpozytywne(X,Y)).
-
-pamietaj(X,Y,nie) :- assertz(xnegatywne(X,Y)).
+                    pamietaj(X, Y, Reply),
+					(Reply = 't').
+					
+pamietaj(X, Y, 't') :- assertz(zapamietane(X, Y, tak)), !.
+pamietaj(X, Y, _) :- assertz(zapamietane(X, Y, nie)).
 
 wyczysc_fakty :- write('Przycisnij cos aby wyjsc'), nl,
-                    retractall(xpozytywne(_,_)),
-                    retractall(xnegatywne(_,_)),
+                    retractall(xpytaj(_,_)),
+                    retractall(xpytaj(_,_)),
                     get_char(_).
                     
 wykonaj :- powinien_mieszkac_w(X), !,
