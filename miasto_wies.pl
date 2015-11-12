48520d1cd92d66bee562b607d94a50ebd7018980
Desmond14 <s.lakomy@gmail.com>
2015-11-12 23:23:51 +0100

    Added more rules

--- miasto_wies.pl
+++ miasto_wies.pl
@@ -2,7 +2,8 @@
     zapamietane/3.
 
 powinien_mieszkac_w(ustrzyki_gorne) :- woli_mieszkac(na_wsi),
-                        wskazana_bliskosc(gory).
+                        wskazana_bliskosc(gory),
+						lubi(odosobnienie).
                         
 powinien_mieszkac_w(krzyze) :- woli_mieszkac(na_wsi),
                         wskazana_bliskosc(woda).
@@ -11,10 +12,26 @@ powinien_mieszkac_w(gdansk) :- woli_mieszkac(w_miescie),
                         wskazana_bliskosc(woda).
                         
 powinien_mieszkac_w(krakow) :- woli_mieszkac(w_miescie),
+						lubi(miasta_turystyczne),
                         wskazana_bliskosc(gory).
 
 powinien_mieszkac_w(warszawa) :- woli_mieszkac(w_miescie),
-                        preferuje_dojazd(komunikacja_miejska).				
+                        preferuje_dojazd(komunikacja_miejska).	
+
+powinien_mieszkac_w(rzeszow) :- woli_mieszkac(w_miescie),
+							preferuje_dojazd(auto).
+powinien_mieszkac_w(rzeszow) :- woli_mieszkac(w_miescie),
+							wskazana_bliskosc(gory).
+							
+powinien_mieszkac_w(zakopane) :- wskazana_bliskosc(gory),
+								pytaj(czy, nie_przeszkadza_duzy_ruch),
+								pytaj(czy, nie_przeszkadzaja_tlumy).
+								
+powinien_mieszkac_w(sandomierz) :- lubi(miasta_turystyczne),
+									pytaj(czy, lubisz_serial_ojciesz_mateusz).
+
+powinien_mieszkac_w(wroclaw) :- woli_mieszkac(w_miescie),
+								pytaj(czy, zalezy_na_bliskosci_duzego_zoo).
 
 woli_mieszkac(na_wsi) :- moze_mieszkac(na_wsi), spelnia_warunki(na_wsi).
 
@@ -35,9 +52,19 @@ wskazana_bliskosc(gory) :- pytaj(czy, uprawiasz_sporty_zimowe).
 wskazana_bliskosc(woda) :- pytaj(czy, lubisz_wypoczywac_na_plazy).
 wskazana_bliskosc(woda) :- pytaj(czy, zeglujesz).
 
-preferuje_dojazd(komunikacja_miejska) :- pytaj(czy, lubisz_jezdzic_autem).
+preferuje_dojazd(komunikacja_miejska) :- \+pytaj(czy, lubisz_jezdzic_autem).
 preferuje_dojazd(komunikacja_miejska) :- pytaj(czy, nie_przeszkadzaja_tlumy),
 									     pytaj(czy, lubisz_czytac_ksiazki).
+										 
+lubi(miasta_turystyczne) :- pytaj(czy, nie_przeszkadzaja_tlumy),
+							pytaj(czy, lubisz_zwiedzac_zabytki),
+							pytaj(czy, lubisz_zwiedzac_muzea).
+							
+lubi(odosobnienie) :- \+pytaj(czy, nie_przeszkadzaja_tlumy),
+                        pytaj(czy, nie_przeszkadza_brak_lub_slaby_zasieg_telefoniczny).
+										 
+preferuje_dojazd(auto) :- pytaj(czy, lubisz_jezdzic_autem),
+                          pytaj(czy, masz_prawo_jazdy).
 
 pytaj(X,Y) :- zapamietane(X, Y, tak),
               !.
@@ -55,8 +82,7 @@ pamietaj(X, Y, 't') :- assertz(zapamietane(X, Y, tak)), !.
 pamietaj(X, Y, _) :- assertz(zapamietane(X, Y, nie)).
 
 wyczysc_fakty :- write('Przycisnij cos aby wyjsc'), nl,
-                    retractall(xpytaj(_,_)),
-                    retractall(xpytaj(_,_)),
+                    retractall(zapamietane(_,_,_)),
                     get_char(_).
                     
 wykonaj :- powinien_mieszkac_w(X), !,
