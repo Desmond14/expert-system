309653bf0a8fa1e0f6152ba324ff907b6e338503
Desmond14 <s.lakomy@gmail.com>
2015-11-19 20:49:28 +0100

    Added enough rules to meet required criteria

--- miasto_wies.pl
+++ miasto_wies.pl
@@ -5,6 +5,10 @@ powinien_mieszkac_w(ustrzyki_gorne) :- woli_mieszkac(na_wsi),
                         wskazana_bliskosc(gory),
 						lubi(odosobnienie).
                         
+powinien_mieszkac_w(zawoja) :- woli_mieszkac(na_wsi),
+                        wskazana_bliskosc(gory),
+						\+lubi(odosobnienie).
+						
 powinien_mieszkac_w(krzyze) :- woli_mieszkac(na_wsi),
                         wskazana_bliskosc(woda).
                         
@@ -16,7 +20,8 @@ powinien_mieszkac_w(krakow) :- woli_mieszkac(w_miescie),
                         wskazana_bliskosc(gory).
 
 powinien_mieszkac_w(warszawa) :- woli_mieszkac(w_miescie),
-                        preferuje_dojazd(komunikacja_miejska).	
+                        preferuje_dojazd(komunikacja_miejska),
+						przeklada_mozliwosci_nad_spokoj.
 
 powinien_mieszkac_w(rzeszow) :- woli_mieszkac(w_miescie),
 							preferuje_dojazd(auto).
@@ -28,10 +33,17 @@ powinien_mieszkac_w(zakopane) :- wskazana_bliskosc(gory),
 								pytaj(czy, nie_przeszkadzaja_tlumy).
 								
 powinien_mieszkac_w(sandomierz) :- lubi(miasta_turystyczne),
-									pytaj(czy, lubisz_serial_ojciesz_mateusz).
+									zalezy_aby_mieszkac(na_wschodzie).
+powinien_mieszkac_w(sandomierz) :- lubi(miasta_turystyczne),
+									przeklada_spokoj_nad_mozliwosci.
+									
+powinien_mieszkac_w(poznan) :- woli_mieszkac(w_miescie),
+								przeklada_spokoj_nad_mozliwosci,
+								\+lubi(miasta_turystyczne).
 
 powinien_mieszkac_w(wroclaw) :- woli_mieszkac(w_miescie),
-								pytaj(czy, zalezy_na_bliskosci_duzego_zoo).
+								zalezy_aby_mieszkac(na_zachodzie),
+								przeklada_mozliwosci_nad_spokoj.
 
 woli_mieszkac(na_wsi) :- moze_mieszkac(na_wsi), spelnia_warunki(na_wsi).
 
@@ -65,6 +77,18 @@ lubi(odosobnienie) :- \+pytaj(czy, nie_przeszkadzaja_tlumy),
 										 
 preferuje_dojazd(auto) :- pytaj(czy, lubisz_jezdzic_autem),
                           pytaj(czy, masz_prawo_jazdy).
+						  
+zalezy_aby_mieszkac(na_wschodzie) :- \+pytaj(czy, zalezy_aby_mieszkac_na_zachodzie),
+									   pytaj(czy, zalezy_aby_mieszkac_na_wschodzie).
+						
+zalezy_aby_mieszkac(na_zachodzie) :- \+pytaj(czy, zalezy_aby_mieszkac_na_wschodzie),
+									   pytaj(czy, zalezy_aby_mieszkac_na_zachodzie).
+						
+przeklada_spokoj_nad_mozliwosci :- pytaj(czy, bardziej_od_mozliwosci_rozwoju_zawodowego_cenisz_bliskosc_zieleni),
+                                   pytaj(czy, lubisz_gdy_miasto_noca_zamiera).
+								   
+przeklada_mozliwosci_nad_spokoj :- pytaj(czy, lubisz_gdy_miasto_noca_tetni_zyciem),
+								   \+pytaj(czy, bardziej_od_mozliwosci_rozwoju_zawodowego_cenisz_bliskosc_zieleni).
 
 pytaj(X,Y) :- zapamietane(X, Y, tak),
               !.
