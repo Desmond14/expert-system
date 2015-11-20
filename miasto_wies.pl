8c8df545bb6e6f58bf636b7848b6f1caa1b1bfa3
lukpecyn <lukpecyn@poczta.onet.pl>
2015-11-20 10:04:26 +0100

    Added JPL library. New "jpl_pytaj" - modified "pytaj" so they used JPL library. Changed "pytaj" to "jpl_pytaj".

--- miasto_wies.pl
+++ miasto_wies.pl
@@ -1,20 +1,22 @@
+:- use_module(library(jpl)).
+
 :- dynamic
     zapamietane/2.
 
 powinien_mieszkac_w(ustrzyki_gorne) :- woli_mieszkac(na_wsi),
                         wskazana_bliskosc(gory),
 						lubi(odosobnienie).
-                        
+
 powinien_mieszkac_w(zawoja) :- woli_mieszkac(na_wsi),
                         wskazana_bliskosc(gory),
 						\+lubi(odosobnienie).
-						
+
 powinien_mieszkac_w(krzyze) :- woli_mieszkac(na_wsi),
                         wskazana_bliskosc(woda).
-                        
+
 powinien_mieszkac_w(gdansk) :- woli_mieszkac(w_miescie),
                         wskazana_bliskosc(woda).
-                        
+
 powinien_mieszkac_w(krakow) :- woli_mieszkac(w_miescie),
 						lubi(miasta_turystyczne),
                         wskazana_bliskosc(gory).
@@ -27,16 +29,16 @@ powinien_mieszkac_w(rzeszow) :- woli_mieszkac(w_miescie),
 							preferuje_dojazd(auto).
 powinien_mieszkac_w(rzeszow) :- woli_mieszkac(w_miescie),
 							wskazana_bliskosc(gory).
-							
+
 powinien_mieszkac_w(zakopane) :- wskazana_bliskosc(gory),
 								pozytywne(nie_przeszkadza_duzy_ruch),
 								pozytywne(nie_przeszkadzaja_tlumy).
-								
+
 powinien_mieszkac_w(sandomierz) :- lubi(miasta_turystyczne),
 									zalezy_aby_mieszkac(na_wschodzie).
 powinien_mieszkac_w(sandomierz) :- lubi(miasta_turystyczne),
 									przeklada_spokoj_nad_mozliwosci.
-									
+
 powinien_mieszkac_w(poznan) :- woli_mieszkac(w_miescie),
 								przeklada_spokoj_nad_mozliwosci,
 								\+lubi(miasta_turystyczne).
@@ -67,55 +69,69 @@ wskazana_bliskosc(woda) :- pozytywne(zeglujesz).
 preferuje_dojazd(komunikacja_miejska) :- negatywne(lubisz_jezdzic_autem).
 preferuje_dojazd(komunikacja_miejska) :- pozytywne(nie_przeszkadzaja_tlumy),
 									     pozytywne(lubisz_czytac_ksiazki).
-										 
+
 lubi(miasta_turystyczne) :- pozytywne(nie_przeszkadzaja_tlumy),
 							pozytywne(lubisz_zwiedzac_zabytki),
 							pozytywne(lubisz_zwiedzac_muzea).
-							
+
 lubi(odosobnienie) :- negatywne(nie_przeszkadzaja_tlumy),
                         pozytywne(nie_przeszkadza_brak_lub_slaby_zasieg_telefoniczny).
-										 
+
 preferuje_dojazd(auto) :- pozytywne(lubisz_jezdzic_autem),
                           pozytywne(masz_prawo_jazdy).
-						  
+
 zalezy_aby_mieszkac(na_wschodzie) :- negatywne(zalezy_aby_mieszkac_na_zachodzie),
 									   pozytywne(zalezy_aby_mieszkac_na_wschodzie).
-						
+
 zalezy_aby_mieszkac(na_zachodzie) :- negatywne(zalezy_aby_mieszkac_na_wschodzie),
 									   pozytywne(zalezy_aby_mieszkac_na_zachodzie).
-						
+
 przeklada_spokoj_nad_mozliwosci :- pozytywne(bardziej_od_mozliwosci_rozwoju_zawodowego_cenisz_bliskosc_zieleni),
                                    pozytywne(lubisz_gdy_miasto_noca_zamiera).
-								   
+
 przeklada_mozliwosci_nad_spokoj :- pozytywne(lubisz_gdy_miasto_noca_tetni_zyciem),
 								   negatywne(bardziej_od_mozliwosci_rozwoju_zawodowego_cenisz_bliskosc_zieleni).
 
 negatywne(X) :- \+pozytywne(X).
-								   
+
 pozytywne(X) :- zapamietane(X, tak),
               !.
-			  
+
 pozytywne(X) :- zapamietane(X, _),
               !,
 			  fail.
-			  
-pozytywne(X) :- pytaj(X).
-                    
-pytaj(X) :- format('Czy ~w ? (t/n)~n',[X,Y]),
+
+pozytywne(X) :- jpl_pytaj(X).
+
+pytaj(X) :- format('Czy ~w ? (t/n)~n',[X]),
                     read(Reply),
                     pamietaj(X, Reply),
-					(Reply = 't').
-					
+		    (Reply = 't').
+
+jpl_pytaj(X) :-
+	atom_concat('Czy ',X,Q1),
+	atom_concat(Q1,'?',Q),
+	jpl_new('javax.swing.JFrame', ['frame with dialog'], F),
+	jpl_call(F, setVisible, [@(false)], _),
+	jpl_call('javax.swing.JOptionPane', showConfirmDialog, [F,Q], N),
+	jpl_call(F, dispose, [], _),
+	(	N == 0
+	->	Reply='t'
+	;	Reply='n'
+	),
+	pamietaj(X,Reply),
+	(Reply = 't').
+
 pamietaj(X, 't') :- assertz(zapamietane(X, tak)), !.
 pamietaj(X, _) :- assertz(zapamietane(X, nie)).
 
 wyczysc_fakty :- write('Przycisnij cos aby wyjsc'), nl,
                     retractall(zapamietane(_,_,_)),
                     get_char(_).
-                    
+
 wykonaj :- powinien_mieszkac_w(X), !,
             format('~nTwoim wymarzonym miejscem do zycia moze byc ~w', X),
             nl, wyczysc_fakty.
-            
+
 wykonaj :- write('Nie jestem w stanie znalezc miejsca do zycia dla ciebie'), nl,
             wyczysc_fakty.
